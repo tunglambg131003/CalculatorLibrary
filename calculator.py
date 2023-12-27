6461c7b8b418728120a0a7f6c396f803ab0cea45
nguyentunglam13102003 <104893299+nguyentunglam13102003@users.noreply.github.com>
2023-12-27 14:23:22 +0700

    Add functions

--- calculator.py
+++ calculator.py
@@ -4,11 +4,14 @@ Calculator library containing basic math operations.
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
 
+
 def multiply(first_term, second_term):
     return first_term * second_term
+
 
 def division(first_term, second_term):
      return first_term // second_term
--- test_calculator.py
+++ test_calculator.py
@@ -7,14 +7,18 @@ import calculator
 
 class TestCalculator:
 
+
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
+
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
+
     def test_multiplication(self):
-      assert 100 == calculator.multiply(10, 10)
+        assert 100 == calculator.multiply(10, 10)
+
 
     def test_division(self):
-      assert 1 == calculator.division(10, 10)
+        assert 1 == calculator.division(10, 10)
